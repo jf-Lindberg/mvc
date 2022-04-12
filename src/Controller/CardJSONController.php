f3cce81af12c53689c682f0685af71994b0d8d2b
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-04-12 12:18:36 +0200

    Removed unnecessary module from CardJSONController

--- src/Controller/CardJSONController.php
+++ src/Controller/CardJSONController.php
@@ -6,7 +6,6 @@ use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
