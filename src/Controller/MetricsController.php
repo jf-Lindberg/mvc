ff55f16081e736efad2caed3915eb80e3e2c5347
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-05-25 18:35:13 +0200

    kmom06 start

---
+++ src/Controller/MetricsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MetricsController extends AbstractController
+{
+    /**
+     * @Route("/metrics", name="metrics-home")
+     */
+    public function home(): Response
+    {
+        $finder = new Finder();
+        $finder->files()->in("../texter/metrics")->name('*.md');
+        $texts = [];
+        foreach ($finder as $file) {
+            $contents = $file->getContents();
+            $texts[] = $contents;
+        }
+
+        $data = [
+            "title" => "Metrics",
+            'texts' => $texts
+        ];
+        return $this->render('metrics/home.html.twig', $data);
+    }
+}
